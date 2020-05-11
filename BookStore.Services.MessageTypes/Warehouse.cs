96dbe851db84af2c3e426111a4bbb0c2f3070b88
pahor-alt <pahoralt@gmail.com>
2020-05-11 18:18:59 +1000

    Overhead for new warehouse service

---
+++ BookStore.Services.MessageTypes/Warehouse.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Services.MessageTypes
+{
+    class Warehouse : MessageType
+    {
+        Dictionary<string, string> Stocks { get; set; }
+    }
+}
